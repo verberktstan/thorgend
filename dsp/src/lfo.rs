5f42909659b00bb7743a4b7d363e1c0025b3372b
Stan <stan@withflow.com>
2026-05-06 21:22:03 +0200

    Implement Sine LFO unit

---
+++ dsp/src/lfo.rs
@@ -0,0 +1,19 @@
+pub struct Lfo {
+  sample_rate: f32,
+  phase: f32,
+}
+
+impl Lfo {
+  pub fn new(sample_rate: f32) -> Self {
+    Self { sample_rate, phase: 0.0 }
+  }
+
+  pub fn process(&mut self, frequency: f32) -> f32 {
+    let out = (self.phase * std::f32::consts::TAU).sin();
+    self.phase += frequency / self.sample_rate;
+    if self.phase >= 1.0 {
+      self.phase -= 1.0;
+    }
+    out
+  }
+}
